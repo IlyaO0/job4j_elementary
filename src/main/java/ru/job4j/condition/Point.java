4f56f63b6dec42ca4343f56aa82183741fc794c0
IlyaO0 <oleinik.i.v@gmail.com>
2022-04-26 23:03:06 +0300

    Вычисление расстояния между двумя точками

--- src/main/java/ru/job4j/condition/Point.java
+++ src/main/java/ru/job4j/condition/Point.java
@@ -12,7 +12,7 @@ public class Point {
     }
 
     public static void main(String[] args) {
-        double result = Point.distance(0, 0, 2, 0);
-        System.out.println("result (0, 0) to (2, 0) " + result);
+        double result = Point.distance(5, 2, 9, 2);
+        System.out.println("result (5, 2) to (9, 2) " + result);
     }
 }
