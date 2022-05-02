831ff439a4579280ad3bd0191e66a5c026fb9424
IlyaO0 <oleinik.i.v@gmail.com>
2022-05-02 11:56:54 +0300

    Входные данные в тесте [#297750 #295886] Исправление

--- src/test/java/ru/job4j/condition/X2Test.java
+++ src/test/java/ru/job4j/condition/X2Test.java
@@ -28,12 +28,12 @@ public class X2Test {
     }
 
     @Test
-    public void whenA0B1C1X1Then0() {
+    public void whenA0B1C1X1Then2() {
         int a = 0;
-        int b = 0;
-        int c = 0;
-        int x = 2;
-        int expected = 0;
+        int b = 1;
+        int c = 1;
+        int x = 1;
+        int expected = 2;
         int rsl = X2.calc(a, b, c, x);
         Assert.assertEquals(expected, rsl);
     }
